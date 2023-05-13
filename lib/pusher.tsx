058e7c3664d012f946684ff96c895f13fcadb42e
VICTORY-PC <tuduov@gmail.com>
2023-05-13 16:35:44 +0100

    removed auth creds

--- lib/pusher.tsx
+++ lib/pusher.tsx
@@ -1,14 +1,11 @@
 import Pusher from "pusher";
 
 const pusher = new Pusher({
-  // appId: process.env.app_id as string,
-  // key: process.env.key as string,
-  // secret: process.env.secret as string,
-  // cluster: process.env.cluster as string,
-  appId: "1593503",
-  key: "93611100d62ff293233c",
-  secret: "349da1e31a8d9b4a74b9",
-  cluster: "mt1",
+  appId: process.env.app_id as string,
+  key: process.env.key as string,
+  secret: process.env.secret as string,
+  cluster: process.env.cluster as string,
+
   useTLS: true,
 });
 
