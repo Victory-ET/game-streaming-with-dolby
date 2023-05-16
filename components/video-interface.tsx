3c36ae7aff82064f6aa13478b359cacd616c9216
VICTORY-PC <tuduov@gmail.com>
2023-05-16 10:42:07 +0100

    integrating dolby.ioe

--- components/video-interface.tsx
+++ components/video-interface.tsx
@@ -1,6 +1,77 @@
 import React from "react";
+// import vortex
+import VoxeetSDK from "@voxeet/voxeet-web-sdk";
+// import useState
+import { useState } from "react";
+import { PeerConnection } from "@millicast/sdk";
+import { Director, Publish, View } from "@millicast/sdk";
 
 const VideoInterface = () => {
+  // state to manage screenshare
+  // const [isSharing, setIsSharing] = useState(false);
+  // const serverURL = "";
+
+  // const capabilities = PeerConnection.getCapabilities("video");
+  // console.log(capabilities);
+
+  //   // Publishing Options
+  //   const broadcastOptions = {
+  //     mediaStream: mediaStream,
+  //   };
+
+  //   // Start broadcast
+  //   try {
+  //     await publisher.connect(broadcastOptions);
+  //   } catch (e) {
+  //     console.error("Connection failed, handle error", e);
+  //   }
+  // };
+  // getCam();
+
+  const handleScreenShare = async () => {
+    const displayMediaOptions = {
+      video: {
+        displaySurface: "window",
+      },
+      audio: false,
+    };
+
+    const screenCapture = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
+
+    const broadcastOptions = {
+      mediaStream: screenCapture,
+    };
+
+    const tokenGenerator = () =>
+      Director.getPublisher({
+        token:
+          "4811b659922ebd118b7fe41cedb5f77662a37215b462d9807b3a3dfac0afe012",
+        streamName: "myStreamName",
+      });
+    const publisher = new Publish("myStreamName", tokenGenerator);
+    try {
+      const mediaStream = await publisher.connect(broadcastOptions);
+    } catch (e) {
+      console.error("Connection failed, handle error", e);
+    }
+
+    // Create callback to generate a new token
+    const ViewtokenGenerator = () =>
+      Director.getSubscriber({
+        streamName: "myStreamName",
+        streamAccountId: "875058156",
+        subscriberToken: "27df13e796d7b2bea982d375ad813d7fc79b6fb52de23aca267a874e624990af", // Optional: This token is needed if you're subscribing to a secure stream,
+        // This token should be provided by the publish owner.
+      });
+
+    // Create Millicast instance
+    const millicastView = new View("publish-stream-name", ViewtokenGenerator);
+  };
+
+  handleScreenShare();
+
   return (
     <div className="video-interface">
       {/* This componnent will house the stream sharing and also display information regarding it */}
