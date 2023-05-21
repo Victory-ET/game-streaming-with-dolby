44fb589ad1269cc7788c048b1684c0864167a51b
VICTORY-PC <tuduov@gmail.com>
2023-05-21 09:27:01 +0100

    added participant numbers

--- components/video-interface.tsx
+++ components/video-interface.tsx
@@ -14,27 +14,17 @@ type UserProps = {
 
 const VideoInterface = ({ username, role }: UserProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const APP_KEY = "9KshJ_n7Xfb5xSM5uZN0Eg==";
-  const APP_SECRET = "pqeVDDQeyZKdDuU9Hl8Q4Oc5MFV0onwhgTFprUzdzuM=";
-  const PUBLISH_TOKEN =
-    "4811b659922ebd118b7fe41cedb5f77662a37215b462d9807b3a3dfac0afe012";
-  const PUBLISH_STREAM_NAME = "myStreamName";
-  const STREAM_ID = "TeTVWH";
 
-  const [screenCapture, setScreenCapture] = useState<MediaStream>();
   const userRole = role ? role : "Streamer";
-
-
-
 
   // alternative attempt with Vorteex insttead of Millicast
   const [name, setName] = useState<string>("");
+  const [participants, setParticipants] = useState<any[]>([]);
   const conferenceAliasInput = "gaming-room";
   useEffect(() => {
     const main = async () => {
       // Generate a client access token from the Dolby.io dashboard and insert into access_token variable
-      let access_token =
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkb2xieS5pbyIsImlhdCI6MTY4NDYwODYxNSwic3ViIjoiOUtzaEpfbjdYZmI1eFNNNXVaTjBFZz09IiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9DVVNUT01FUiJdLCJ0YXJnZXQiOiJzZXNzaW9uIiwib2lkIjoiODczZmFmYmMtMDYzOC00YWUzLWIyZGYtY2Q4MWRiYWM3MWY1IiwiYWlkIjoiMTM4Nzg4ZTMtYjU3OS00YmZmLTk3M2ItODk0OTk1OTU1NDYzIiwiYmlkIjoiOGEzNjhjZWU4NzI4MzQyNDAxODcyZDUzNmMxMjE4ZGMiLCJleHAiOjE2ODQ2OTUwMTV9.ZuxzbgOEf0PLwsF1THVariBEq58E74OH8goYQ8VeUiQgvrRpT5Mv6k167ZeY6Br-71DPMsdzMVAdBcdWm9w_qg";
+      let access_token = process.env.NEXT_PUBLIC_CLIENT_ACCESS_TOKEN as string;
       VoxeetSDK.initializeToken(access_token, (isExpired: any) => {
         return new Promise((resolve, reject) => {
           if (isExpired) {
@@ -44,7 +34,6 @@ const VideoInterface = ({ username, role }: UserProps) => {
           }
         });
       });
-
 
       try {
         // Open the session
@@ -58,6 +47,12 @@ const VideoInterface = ({ username, role }: UserProps) => {
           .create({ alias: conferenceAliasInput })
           .then((conference) => VoxeetSDK.conference.join(conference, {}))
           .catch((err) => console.error(err));
+
+        // Listen to participant events
+        VoxeetSDK.conference.on("participantAdded", (participant) => {
+          setParticipants((prevParticipants) => [...prevParticipants, participant]);
+        }
+        );
       } catch (e) {
         alert("Something went wrong : " + e);
       }
@@ -68,16 +63,16 @@ const VideoInterface = ({ username, role }: UserProps) => {
 
   const sharingScreen = async () => {
     if (userRole.toLowerCase() === "streamer") {
-    VoxeetSDK.conference
-      .startScreenShare()
-      .then(() => {
-        console.log("Screen sharing started");
-        // const screenCapture = await navigator.mediaDevices.getDisplayMedia(
-        //   displayMediaOptions
-        // );
-        displayStream(videoRef.current as HTMLVideoElement);
-      })
-      .catch((err) => console.error(err));
+      VoxeetSDK.conference
+        .startScreenShare()
+        .then(() => {
+          console.log("Screen sharing started");
+          // const screenCapture = await navigator.mediaDevices.getDisplayMedia(
+          //   displayMediaOptions
+          // );
+          displayStream(videoRef.current as HTMLVideoElement);
+        })
+        .catch((err) => console.error(err));
     } else {
       alert("welcome viewer");
       displayStream(videoRef.current as HTMLVideoElement);
@@ -98,7 +93,6 @@ const VideoInterface = ({ username, role }: UserProps) => {
     });
   };
 
-
   return (
     <div className="video-interface">
       {/* This componnent will house the stream sharing and also display information regarding it */}
@@ -106,7 +100,7 @@ const VideoInterface = ({ username, role }: UserProps) => {
         {/* view area */}
         <div className="video-interface__view-area__viewers">
           {/* number of viewers */}
-          <p>500 Watching, you are logged in as {name}</p>
+          <p>{participants.length} Watching, you are logged in as {name}</p>
         </div>
         <video
           className="h-full relative w-full"
